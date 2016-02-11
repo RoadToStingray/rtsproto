ee9d4df00c73b70e585365d3fb05b091f0b6120e
Tatsuya Uehara <uehara.tatsuya@gmail.com>
2016-02-12 01:20:02 +0900

    PPKで走り、ジャンプ、ダブルジャンプ testmap2
    パピコ版追加しました。レベルを別にしてます。

--- prototype1/script/lua/flow_callbacks.lua
+++ prototype1/script/lua/flow_callbacks.lua
@@ -1,4 +1,6 @@
 ProjectFlowCallbacks = ProjectFlowCallbacks or {}
+local Application = stingray.Application
+local Level = s3d.Level
 
 -- Example custom project flow node callback. Prints a message.
 -- The parameter t contains the node inputs, and node outputs can
@@ -6,4 +8,12 @@ ProjectFlowCallbacks = ProjectFlowCallbacks or {}
 function ProjectFlowCallbacks.example(t)
 	local message = t.Text or ""
 	print("Example Node Message: " .. message)
+end
+
+
+function ProjectFlowCallbacks.level_event(t)
+    local message = t.text or "" -- note all custom node input variable names are converted to lower case
+    local world = Application.flow_callback_context_world()
+    local level = Application.flow_callback_context_level()
+    flow_var = Level.trigger_event(level, message)
 end
